1fc5b692b51ef2e038380ab22d9b1a9de2ae0424
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-08 18:27:47 +0100

    fix: add try catch

--- update.php
+++ update.php
@@ -3,7 +3,18 @@ include 'db.php';
 $id = $_POST['id'];
 $name = $_POST['name'];
 $score = $_POST['score'];
-$sql = "update demo_table set name='$name', score='$score' where id=$id";
-$result = $conn->query($sql);
-$conn->close();
-header("location: index.php");
+
+try {
+    $conn->stmt_init();
+    $stmt = $conn->prepare('UPDATE users SET name = ?, score = ? WHERE id = ?');
+    $stmt->bind_param('sii', $name, $score, $id);
+    $stmt->execute();
+
+    if ($stmt->affected_rows == 0)
+        exit('Error al actualizar el usuario');
+
+    $stmt->close();
+    header("location: index.php");
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
