a6dcee628cc952cf7c9f4a94cb7374487d7b9146
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-08 19:25:39 +0100

    fix: delete warning

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        body,
-        input {
+        body, input {
             font-family: system-ui;
             font-size: 16px;
         }
@@ -13,35 +14,23 @@
 </head>
 
 <body>
-    <table class="table">
-        <tbody>
-            <?php include 'read.php'; ?>
-        </tbody>
-    </table>
+    <div class="container">
+        <h1>CRUD</h1>
 
-    <form action="create.php" method="POST">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name">
-        <label for="score">Score:</label>
-        <input type="number" id="score" name="score">
-        <input type="submit" value="Add"></input>
-    </form>
-</body>
+        <table class="table">
+            <tbody>
+                <?php include 'read.php'; ?>
+            </tbody>
+        </table>
 
-</html>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
+        <form action="create.php" method="POST">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name">
+            <label for="score">Score:</label>
+            <input type="number" id="score" name="score">
+            <input type="submit" value="Add">
+        </form>
+    </div>
 </body>
 
 </html>
